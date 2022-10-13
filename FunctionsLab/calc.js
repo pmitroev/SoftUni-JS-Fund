f63a47a36c9b4640af4140d52cb658b370440eb4
pmitroev <presalv1233@gmail.com>
2022-10-13 21:28:59 +0300

    simpCalc

--- FunctionsLab/calc.js
+++ FunctionsLab/calc.js
@@ -1,16 +1,24 @@
 function solve(n1, n2, func) {
-    let final = 0;
-    switch (func) {
-        case 'multiply':
-            final = n1 * n2; break;
-        case 'divide':
-            final = n1 / n2; break;
-        case 'add':
-            final = n1 + n2; break;
-        case 'subtract':
-            final = n1 - n2; break;
+    // let final = 0;
+    // switch (func) {
+    //     case 'multiply':
+    //         final = n1 * n2; break;
+    //     case 'divide':
+    //         final = n1 / n2; break;
+    //     case 'add':
+    //         final = n1 + n2; break;
+    //     case 'subtract':
+    //         final = n1 - n2; break;
+    // }
+    // console.log(final)
+    let ops = {
+        multiply: n1 * n2,
+        divide: n1 / n2,
+        add: n1 + n2,
+        subtract: n1 - n2
     }
-    console.log(final)
+    console.log(ops[func])
 }
+
 
 solve(5,5,'multiply')
